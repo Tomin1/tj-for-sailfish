09d02ed94f668152cad4498dec06d28856dbea70
Tomi Leppänen <tomi.leppanen@telemail.fi>
2014-07-25 19:04:06 +0300

    Lisätty päivän pokemon
    Yleisön pyynnöstä

--- qml/tj/script.js
+++ qml/tj/script.js
@@ -79,10 +79,18 @@ function Era(str) {
             return palvelusaika - tj;
         return palvelusaika - this.tj();
     };
+
+    this.pokemon = function(tj) {
+        if (tj === null)
+            tj = this.tj();
+        if (tj === 0)
+            return "N/A";
+        return pokemonit[tj];
+    }
 }
 
 function set_values(palvelukseenastumispaiva, kotiutumispaiva,
-                    tanaan_jaljella, palvelusta_kayty) {
+                    tanaan_jaljella, palvelusta_kayty, paivan_pokemon) {
     if (vuosi === null || era === null || palvelusaika === null)
         return;
     var sp = get_saapumisera();
@@ -91,6 +99,7 @@ function set_values(palvelukseenastumispaiva, kotiutumispaiva,
     tj = sp.tj(palvelusaika);
     tanaan_jaljella.value = tj;
     palvelusta_kayty.value = sp.palvelusta_kayty(palvelusaika, tj);
+    paivan_pokemon.value = sp.pokemon(tj);
 }
 
 function get_date_string(date) {
@@ -105,3 +114,62 @@ function get_tj_string() {
         return tj.toString() + (tj != 1 ? " aamua" : " aamu");
     return "N/A";
 }
+
+var pokemonit = [
+    null, "Bulbasaur", "Ivysaur", "Venusaur", "Charmander", "Charmeleon",
+    "Charizard", "Squirtle", "Wartortle", "Blastoise", "Caterpie", "Metapod",
+    "Butterfree", "Weedle", "Kakuna", "Beedrill", "Pidgey", "Pidgeotto",
+    "Pidgeot", "Rattata", "Raticate", "Spearow", "Fearow", "Ekans", "Arbok",
+    "Pikachu", "Raichu", "Sandshrew", "Sandslash", "Nidoran♀", "Nidorina",
+    "Nidoqueen", "Nidoran♂", "Nidorino", "Nidoking", "Clefairy", "Clefable",
+    "Vulpix", "Ninetales", "Jigglypuff", "Wigglytuff", "Zubat", "Golbat",
+    "Oddish", "Gloom", "Vileplume", "Paras", "Parasect", "Venonat", "Venomoth",
+    "Diglett", "Dugtrio", "Meowth", "Persian", "Psyduck", "Golduck", "Mankey",
+    "Primeape", "Growlithe", "Arcanine", "Poliwag", "Poliwhirl", "Poliwrath",
+    "Abra", "Kadabra", "Alakazam", "Machop", "Machoke", "Machamp",
+    "Bellsprout", "Weepinbell", "Victreebel", "Tentacool", "Tentacruel",
+    "Geodude", "Graveler", "Golem", "Ponyta", "Rapidash", "Slowpoke",
+    "Slowbro", "Magnemite", "Magneton", "Farfetch'd", "Doduo", "Dodrio",
+    "Seel", "Dewgong", "Grimer", "Muk", "Shellder", "Cloyster", "Gastly",
+    "Haunter", "Gengar", "Onix", "Drowzee", "Hypno", "Krabby", "Kingler",
+    "Voltorb", "Electrode", "Exeggcute", "Exeggutor", "Cubone", "Marowak",
+    "Hitmonlee", "Hitmonchan", "Lickitung", "Koffing", "Weezing", "Rhyhorn",
+    "Rhydon", "Chansey", "Tangela", "Kangaskhan", "Horsea", "Seadra",
+    "Goldeen", "Seaking", "Staryu", "Starmie", "Mr. Mime", "Scyther", "Jynx",
+    "Electabuzz", "Magmar", "Pinsir", "Tauros", "Magikarp", "Gyarados",
+    "Lapras", "Ditto", "Eevee", "Vaporeon", "Jolteon", "Flareon", "Porygon",
+    "Omanyte", "Omastar", "Kabuto", "Kabutops", "Aerodactyl", "Snorlax",
+    "Articuno", "Zapdos", "Moltres", "Dratini", "Dragonair", "Dragonite",
+    "Mewtwo", "Mew", "Chikorita", "Bayleef", "Meganium", "Cyndaquil",
+    "Quilava", "Typhlosion", "Totodile", "Croconaw", "Feraligatr", "Sentret",
+    "Furret", "Hoothoot", "Noctowl", "Ledyba", "Ledian", "Spinarak", "Ariados",
+    "Crobat", "Chinchou", "Lanturn", "Pichu", "Cleffa", "Igglybuff", "Togepi",
+    "Togetic", "Natu", "Xatu", "Mareep", "Flaaffy", "Ampharos", "Bellossom",
+    "Marill", "Azumarill", "Sudowoodo", "Politoed", "Hoppip", "Skiploom",
+    "Jumpluff", "Aipom", "Sunkern", "Sunflora", "Yanma", "Wooper", "Quagsire",
+    "Espeon", "Umbreon", "Murkrow", "Slowking", "Misdreavus", "Unown",
+    "Wobbuffet", "Girafarig", "Pineco", "Forretress", "Dunsparce", "Gligar",
+    "Steelix", "Snubbull", "Granbull", "Qwilfish", "Scizor", "Shuckle",
+    "Heracross", "Sneasel", "Teddiursa", "Ursaring", "Slugma", "Magcargo",
+    "Swinub", "Piloswine", "Corsola", "Remoraid", "Octillery", "Delibird",
+    "Mantine", "Skarmory", "Houndour", "Houndoom", "Kingdra", "Phanpy",
+    "Donphan", "Porygon2", "Stantler", "Smeargle", "Tyrogue", "Hitmontop",
+    "Smoochum", "Elekid", "Magby", "Miltank", "Blissey", "Raikou", "Entei",
+    "Suicune", "Larvitar", "Pupitar", "Tyranitar", "Lugia", "Ho-oh", "Celebi",
+    "Treecko", "Grovyle", "Sceptile", "Torchic", "Combusken", "Blaziken",
+    "Mudkip", "Marshtomp", "Swampert", "Poochyena", "Mightyena", "Zigzagoon",
+    "Linoone", "Wurmple", "Silcoon", "Beautifly", "Cascoon", "Dustox", "Lotad",
+    "Lombre", "Ludicolo", "Seedot", "Nuzleaf", "Shiftry", "Taillow", "Swellow",
+    "Wingull", "Pelipper", "Ralts", "Kirlia", "Gardevoir", "Surskit",
+    "Masquerain", "Shroomish", "Breloom", "Slakoth", "Vigoroth", "Slaking",
+    "Nincada", "Ninjask", "Shedinja", "Whismur", "Loudred", "Exploud",
+    "Makuhita", "Hariyama", "Azurill", "Nosepass", "Skitty", "Delcatty",
+    "Sableye", "Mawile", "Aron", "Lairon", "Aggron", "Meditite", "Medicham",
+    "Electrike", "Manectric", "Plusle", "Minun", "Volbeat", "Illumise",
+    "Roselia", "Gulpin", "Swalot", "Carvanha", "Sharpedo", "Wailmer",
+    "Wailord", "Numel", "Camerupt", "Torkoal", "Spoink", "Grumpig", "Spinda",
+    "Trapinch", "Vibrava", "Flygon", "Cacnea", "Cacturne", "Swablu", "Altaria",
+    "Zangoose", "Seviper", "Lunatone", "Solrock", "Barboach", "Whiscash",
+    "Corphish", "Crawdaunt", "Baltoy", "Claydol", "Lileep", "Cradily",
+    "Anorith"
+]
